db0f64cec5a660b9fc589c7d4fcf3a86349f8fbe
Mohak <mohakagr@gmail.com>
2016-04-26 14:50:11 +0530

    Todo list with permanent storage and swipe to delete

--- To-Do-List/FirstViewController.swift
+++ To-Do-List/FirstViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var todoListItems = [String]()
 
+class FirstViewController: UIViewController,UITableViewDelegate {
+
+    @IBOutlet weak var todoListView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            todoListItems = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoListItems.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = todoListItems[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        todoListView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        todoListItems.removeAtIndex(indexPath.row)
+        todoListView.reloadData()
+    }
 
 }
 
--- To-Do-List/SecondViewController.swift
+++ To-Do-List/SecondViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var todoItem: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.todoItem.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,19 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func AddItemCallback(sender: AnyObject) {
+        self.view.endEditing(true);
+        todoListItems.append(todoItem.text!);
+        todoItem.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(todoListItems, forKey: "toDoList")
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        todoItem.resignFirstResponder()
+        return true
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true);
+    }
 }
 
